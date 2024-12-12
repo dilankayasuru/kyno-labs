b13e39f7bcbb7f104f1090b0a86a8a9b5251e720
dilankayasuru <dilankayasuru001@gmail.com>
2024-12-12 08:24:21 +0530

    fix build error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {MouseEventHandler, useState} from "react";
+import {useState} from "react";
 import Link from "next/link";
 import Image from "next/image";
 import MenuClose from "./icons/menuClose";
